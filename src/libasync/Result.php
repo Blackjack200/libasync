e6a5f3f73c28b2c29d99004a0e4f83dccdd4302a
Blackjack200 <lx826444126@gmail.com>
2022-07-16 21:39:25 +0800

    result

---
+++ src/libasync/Result.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace libasync;
+
+use Closure;
+use Throwable;
+
+class Result {
+	/**
+	 * @param Closure<Closure> $caller
+	 * @param Closure<PromiseException|Throwable> $errorCaller
+	 */
+	public function __construct(
+		private Closure $caller,
+		private Closure $errorCaller,
+	) {
+	}
+
+	/**
+	 * @param Closure<$this>|null $c
+	 */
+	public function unwrap(?Closure $c = null) : void {
+		if ($c === null) {
+			$c = static fn() => null;
+		}
+		($this->caller)($c);
+	}
+
+	/**
+	 * @param Closure<string> $c
+	 */
+	public function catch(Closure $c) : self {
+		($this->errorCaller)($c);
+		return $this;
+	}
+
+	/**
+	 * @param Result ...$results
+	 */
+	public function combine(...$results) : Result {
+		if (count($results) < 1) {
+			return $this;
+		}
+		array_unshift($results, $this);
+		return new Result(static function(Closure $func) use ($results) : void {
+			$args = [];
+			$lastFunc = static function(...$a) use ($func, &$args) {
+				$args = array_merge($args, $a);
+				$func(...$args);
+			};
+			for ($i = count($results) - 1; $i > 0; $i--) {
+				$result = $results[$i];
+				$lastFunc = static function(...$a) use ($lastFunc, &$args, $result) {
+					$args = array_merge($args, $a);
+					$result->unwrap($lastFunc);
+				};
+			}
+			$results[array_key_first($results)]->unwrap($lastFunc);
+		}, static fn() => null);
+	}
+
+	public function promise() : PromiseInterface {
+		return (new SyncedPromise())->bind(ResultPromiseCaller::class)->then(function($resolve, $reject, $errorHandler) : void {
+			$this->unwrap(static function(...$args) use ($errorHandler, $resolve) : void {
+				try {
+					$resolve(...$args);
+				} catch (Throwable $err) {
+					$errorHandler($err);
+				}
+			});
+		});
+	}
+
+	private static function empty() : Result {
+		return new Result(static fn(Closure $c) => $c(), static fn() => null);
+	}
+}
---
+++ src/libasync/ResultPromiseCaller.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace libasync;
+
+
+use GlobalLogger;
+use pocketmine\utils\Utils;
+
+class ResultPromiseCaller {
+	private mixed $result;
+	private bool $rejected = true;
+	private ?PromiseException $error = null;
+	protected PromiseInterface $promise;
+
+	public function __construct(PromiseInterface $promise) {
+		$this->promise = $promise;
+	}
+
+	private function onRun() : void {
+		$reject = function(...$reason) : void {
+			$this->result = $reason;
+			$this->onCompletion();
+			throw new InterruptSignal();
+		};
+		$resolve = function(...$reason) : void {
+			$this->rejected = false;
+			$this->result = $reason;
+			$this->onCompletion();
+			throw new InterruptSignal();
+		};
+		$errorHandler = function(\Throwable $err) : void {
+			if (!$err instanceof InterruptSignal) {
+				$this->error = PromiseException::from([$err::class, $err->getMessage(), Utils::printableTrace($err->getTrace()), $err->getCode(), $err->getFile(), $err->getLine()]);
+			}
+		};
+		($this->promise->getAsyncCall())($resolve, $reject, $errorHandler);
+	}
+
+	private function onCompletion() : void {
+		$promise = $this->promise;
+		if ($this->error !== null) {
+			$handler = $promise->getErrorHandler();
+			if ($handler !== null) {
+				$handler($this->error);
+			} else {
+				$this->error->print(GlobalLogger::get());
+			}
+			return;
+		}
+		try {
+			$data = $this->result;
+			if ($this->rejected) {
+				$callbacks = $promise->getRejectedCallbacks();
+			} else {
+				$callbacks = $promise->getFulfillCallbacks();
+			}
+			foreach ($callbacks as $callback) {
+				if (is_iterable($data)) {
+					$callback(...$data);
+				} else {
+					$callback($data);
+				}
+			}
+		} catch (\Throwable $throwable) {
+			GlobalLogger::get()->logException($throwable);
+		}
+	}
+
+	final public function start() : void {
+		$this->onRun();
+	}
+}
