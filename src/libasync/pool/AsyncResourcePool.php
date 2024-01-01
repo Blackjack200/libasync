a92352f2c7c87c25641b8ba5605b2159fee282da
Blackjack200 <lx826444126@gmail.com>
2024-01-01 22:12:51 +0800

    improvements

--- src/libasync/pool/AsyncResourcePool.php
+++ src/libasync/pool/AsyncResourcePool.php
@@ -12,9 +12,10 @@ use libasync\utils\ResourceRef;
  * @implements ResourcePoolInterface<T>
  */
 class AsyncResourcePool implements ResourcePoolInterface {
+	protected int $standardCapacity;
 	/** @var array<string, array{0:\Closure():(T|null), 1:\Closure(T):void, 2:Closure(T,Closure(T):void $push):void} */
 	private array $types = [];
-	private array $resources = [];
+	private array $resourcesHandle = [];
 	private array $queued = [];
 
 	/**
@@ -22,8 +23,9 @@ class AsyncResourcePool implements ResourcePoolInterface {
 	 */
 	public function register(string $type, Closure $prepareFunc, Closure $freeFunc, Closure $recycleFunc) : void {
 		$this->types[$type] = [$prepareFunc, $freeFunc, $recycleFunc];
-		$this->resources[$type] = [];
+		$this->resourcesHandle[$type] = [];
 		$this->queued[$type] = 0;
+		$this->standardCapacity = 2;
 	}
 
 	public function prepare(string $type, int $count) : AwaitResult {
@@ -32,7 +34,7 @@ class AsyncResourcePool implements ResourcePoolInterface {
 		return Await::do(function() use ($count, $type, $prepareFunc) {
 			for ($i = 0; $i < $count; $i++) {
 				$rawRes = $prepareFunc();
-				$this->resources[$type][] = $rawRes;
+				$this->resourcesHandle[$type][] = $rawRes;
 				$this->queued[$type]--;
 				Await::sleep(1);
 			}
@@ -42,23 +44,20 @@ class AsyncResourcePool implements ResourcePoolInterface {
 	public function isRegistered(string $type) : bool { return isset($this->types[$type]); }
 
 	public function select(string $type) : ?ResourceRef {
-		if (!isset($this->resources[$type])) {
+		if (!isset($this->resourcesHandle[$type])) {
 			return null;
 		}
-		$resourceCount = $this->queued[$type] + count($this->resources[$type]);
+		$resourceCount = $this->queued[$type] + count($this->resourcesHandle[$type]);
 		if ($resourceCount === 0) {
-			$this->prepare($type, 5)->panic();
+			$this->prepare($type, $this->standardCapacity)->panic();
 			return null;
 		}
-		if (count($this->resources[$type]) === 0) {
+		if (count($this->resourcesHandle[$type]) === 0) {
 			return null;
 		}
-		if ($resourceCount <= 5) {
-			$this->prepare($type, 5 - $resourceCount)->panic();
-		}
-		$res = array_pop($this->resources[$type]);
+		$res = array_pop($this->resourcesHandle[$type]);
 		$userRecycle = $this->types[$type][2];
-		$push = fn($res) => $this->resources[$type][] = $res;
+		$push = fn($res) => $this->resourcesHandle[$type][] = $res;
 
 		return new ResourceRef($res, $this->types[$type][1], static function($res) use ($push, $userRecycle) : bool {
 			$pushed = false;
@@ -72,7 +71,7 @@ class AsyncResourcePool implements ResourcePoolInterface {
 	}
 
 	public function close() : void {
-		foreach ($this->resources as $type => $rawRes) {
+		foreach ($this->resourcesHandle as $type => $rawRes) {
 			$free = $this->types[$type][1];
 			foreach ($rawRes as $res) {
 				$free($res, true);
@@ -81,9 +80,9 @@ class AsyncResourcePool implements ResourcePoolInterface {
 	}
 
 	public function put(string $type, $resource) : void {
-		if (!isset($this->resources[$type])) {
+		if (!isset($this->resourcesHandle[$type])) {
 			throw new \InvalidArgumentException("Resource type $type is not registered");
 		}
-		$this->resources[$type][] = $resource;
+		$this->resourcesHandle[$type][] = $resource;
 	}
 }
